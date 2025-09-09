719ccc230977b54604f97789b1f435844ab7359d
Clementino Tete <ctete@c2r7s1.42luanda.com>
2025-09-09 09:18:42 +0100

    make e b

---
+++ C05/Bureaucrat.hpp
@@ -0,0 +1,19 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include<iostream>
+#include<string.h>
+#include<exception>
+
+
+class Bureaucrat{
+	
+	private:
+
+
+
+	
+	public:
+	  Bureaucrat();
+	  ~Bureaucrat();
+
+};
