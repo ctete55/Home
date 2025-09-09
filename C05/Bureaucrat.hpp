22f3ce426bf4964d3a280492027489ebbbcafab0
Clementino Tete <ctete@c2r7s1.42luanda.com>
2025-09-09 11:37:19 +0100

    yupi

--- C05/Bureaucrat.hpp
+++ C05/Bureaucrat.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctete <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 09:40:21 by ctete             #+#    #+#             */
+/*   Updated: 2025/09/09 11:30:43 by ctete            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 #include<iostream>
@@ -8,12 +20,23 @@
 class Bureaucrat{
 	
 	private:
-
+	  const std::string name;
+	  int cout;
 
 
 	
 	public:
-	  Bureaucrat();
+	  
+	  class is_higth : public std:: exception{ virtual const char *what()const throw() };
+	  class is_low: public std exception{ virtual const char *what()const throw() };
+
+	  Bureaucrat(std::string &name, int cout);
 	  ~Bureaucrat();
+
+	  std::string getName()const;
+	  int getCout();
+
+	  void incrementCout();
+	  void decrementCout();
 
 };
